615b081cda9d445c077babe01cfe86a2b68a5c96
Leon Piela <s29854@pjwstk.edu.pl>
2024-03-06 13:21:37 +0100

    untitled

---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert an integer");
+        int a = scanner.nextInt();
+    if (a > 0){
+        System.out.println("The number is positive");
+    }
+        else if (a < 0){
+            System.out.println("The number is negative");
+    }
+        else if (a == 0){
+            System.out.println("The value equals 0");
+    }
+
+    }
+}
